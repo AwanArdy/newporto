891a1d597aa647b0c762c233acf5671bc5982e4b
Awan Ardy <muhammadawan46@gmail.com>
2025-05-03 14:11:15 +0800

    new markdown

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,8 +3,11 @@ import { defineCollection, z } from "astro:content";
 const blogCollection = defineCollection({
   schema: z.object({
     title: z.string(),
-    date: z.string(),
     description: z.string(),
+    image: z.string().url(),
+    date: z.string(),
+    category: z.string().optional(),
+    hidden: z.boolean().optional(),
   }),
 });
 
